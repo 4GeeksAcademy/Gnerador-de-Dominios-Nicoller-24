d3da467a30dea8e840fe0558ad13f092da895488
Nicoller-24 <166450090+Nicoller-24@users.noreply.github.com>
2024-08-03 02:55:28 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "his"];
+let adj = ["great", "big", "amazing"];
+let noun = ["jogger", "racoon", "dog", "merchant", "driver"];
+let extension = [".com", ".net", ".gov", ".org", ".us"];
+
+function GeneradorDominios() {
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extension.length; e++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + extension[e]);
+        }
+      }
+    }
+  }
+}
+
+GeneradorDominios();
